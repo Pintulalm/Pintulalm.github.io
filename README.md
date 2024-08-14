082f2780d4bb82f5d5654230f5829880d247228b
Meenapintu <pintulalmee@gmail.com>
2024-08-14 07:29:33 +0000

    Update from https://github.com/Meenapintu/yehigo/commit/5391e7f0a0f011f41cfe39d8588c1b9830b48af2

--- README.md
+++
@@ -1,1 +0,0 @@
-# meenapintu.github.io
---
+++ js/init.js
@@ -0,0 +1,18 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+  
+    $('.slider').slider({
+      indicators:false
+    });
+    $('.materialboxed').materialbox();
+ 
+                 
+   
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+
